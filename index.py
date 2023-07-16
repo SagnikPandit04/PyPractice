dfabcb96893b9f402e731042a4748bce661074ad
SagnikPandit04 <sagnikpandit04@gmail.com>
2023-07-16 19:43:29 +0530

    index

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-mylist = [num for num in range(0,9)]
+length = int(input('Enter the length:\n '))
+breadth = int(input('Enter the breadth:\n '))
+height = int(input('Enter the height:\n '))
+area = length*breadth*height
+print(area)
+
+
