281040e69374cf0538f9dc8c1843ce7f238e8ecd
SagnikPandit04 <sagnikpandit04@gmail.com>
2023-07-18 13:41:31 +0530

    yes

--- index.py
+++ index.py
@@ -3,3 +3,15 @@ for num in my_file:
     print(num)
 
 
+pple = 'it has vitamin A'
+orange = 'it has citric acid'
+banana = 'it has vitamin b'
+question = input('What is your favourite fruit?\n ')
+if question == 'apple':
+    print(apple)
+if question == 'orange':
+    print(orange)
+if question == 'banana':
+    print(banana)
+else:
+    print('you do not have favourite fruit')
