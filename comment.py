c01a491c64021d1a97b6119399a67d3557bf282b
SagnikPandit04 <sagnikpandit04@gmail.com>
2023-07-18 11:43:39 +0530

    var

--- comment.py
+++ comment.py
@@ -1,7 +1,7 @@
-# mylist = 'my name is sagnik pandit'
-# for letter in mylist:
-#     if letter == 'n':
-#         print(letter)
+mylist = 'my name is sagnik pandit'
+for letter in mylist:
+    if letter == 'n':
+        print(letter)
 
 mylist = 'my name is sayoni pandit'
 for letter in mylist:
