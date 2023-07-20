0bf44c1322ae949d561a4c455b3966a24de6d1e0
SagnikPandit04 <sagnikpandit04@gmail.com>
2023-07-20 19:59:20 +0530

    sagnik

---
+++ ignore.py
@@ -0,0 +1,12 @@
+Sayoni = 'I will come with you'
+Sagnik = 'I will not come with you'
+anku = 'where will we go'
+question = input('Whose talk do you want?\n ')
+if question == 'Sayoni':
+    print(Sayoni)
+elif question == 'Sagnik':
+    print(Sagnik)
+elif question == 'anku':
+    print(anku)
+else:
+   print('nothing can be found')
