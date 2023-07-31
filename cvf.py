664e169e266cb8b5f8cf23b3a893427916d92965
SagnikPandit04 <sagnikpandit04@gmail.com>
2023-07-31 16:50:44 +0530

    keyword

---
+++ cvf.py
@@ -0,0 +1,16 @@
+def keyword():
+    question = int(input('what is your result?\n '))
+    if question >90<100:
+        print('exellent')
+    elif question >80<90:
+        print('more than enough')
+    elif question >70<80:
+        print('not bad')
+    else:
+        print('good but can be better')
+
+
+    keyword()
+      
+
+    
--- xyz.py
+++ xyz.py
@@ -1,2 +1,2 @@
 def add_num(num1,num2):
-    add_num(1,2)
+    return num1+num2
