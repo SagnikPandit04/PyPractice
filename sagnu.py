e3e75889471ddd48c92c85ffa42174c547d4f245
SagnikPandit04 <sagnikpandit04@gmail.com>
2024-02-22 18:13:09 +0530

    alternate step

--- sagnu.py
+++ sagnu.py
@@ -1,15 +1,19 @@
+# def main(a,b):
+#     if a%2 == 0 and b%2 == 0:
+#         if a > b:
+#             result = a
+#         else:
+#             result = b
+#     else:
+#         if a<b:
+#             result = a
+#         else:
+#             result = 'b'
+
+#     return result 
+
 def main(a,b):
     if a%2 == 0 and b%2 == 0:
-        if a > b:
-            result = a
-        else:
-            result = b
+        return min(a,b)
     else:
-        if a<b:
-            result = a
-        else:
-            result = b
-
-    return result 
-
-        
+        return max(a,b)
